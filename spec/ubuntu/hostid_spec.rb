cf05b66ce1b487d824b20aa8cb9b658a87778305
Christopher Horrell <christopher@joyent.com>
2015-03-03 16:15:24 -0500

    Updated hosted test for Serverspec v2

--- spec/ubuntu/hostid_spec.rb
+++ spec/ubuntu/hostid_spec.rb
@@ -3,6 +3,6 @@ require 'spec_helper'
 # Ensure the hostid value is unique. See IMAGE-462.
 # https://bugs.launchpad.net/orinda/+bug/1284713
 describe command('hostid') do
-  it { should_not return_stdout "00000000" }
+  its(:stdout) { should_not match "00000000" }
 end
 
