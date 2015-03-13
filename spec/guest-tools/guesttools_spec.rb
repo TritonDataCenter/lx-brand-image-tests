7132749a885d2b02e08363d6d593f80bd457f5f3
Christopher Horrell <christopher@joyent.com>
2015-03-13 09:59:31 -0400

    Clean up test for mdata-* tools

--- spec/guest-tools/guesttools_spec.rb
+++ spec/guest-tools/guesttools_spec.rb
@@ -1,7 +1,5 @@
 require 'spec_helper'
 
-# Since 20140106. See IMAGE-440. Addresses IMAGE-400.
-# Certified Ubuntu images missing mdata-get, etc
 describe file('/usr/sbin/mdata-get') do
   it { should be_file }
 	it { should be_mode 755 }
@@ -21,9 +19,3 @@ describe file('/usr/sbin/mdata-delete') do
   it { should be_file }
   it { should be_mode 755 }
 end
-
-describe file('/lib/smartdc/mdata-get') do
-	it { should be_file }
-	it { should be_linked_to '/usr/sbin/mdata-get' }
-end
-
