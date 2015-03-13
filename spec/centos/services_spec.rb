7a2b56402f3487dc1329c99fc102b16e365f3595
Christopher Horrell <christopher@joyent.com>
2015-03-13 10:12:33 -0400

    Tests for services for CentOS

---
+++ spec/centos/services_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+describe service('crond') do
+  it { should be_enabled }
+end
+
+describe service('rsyslog') do
+  it { should be_enabled }
+end
+
+describe service('sshd') do
+  it { should be_enabled }
+end
+
