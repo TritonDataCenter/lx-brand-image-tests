974fca54dd14298bf53c02d231dbc531f38eb3e1
Christopher Horrell <christopher@joyent.com>
2015-03-15 21:39:00 -0400

    remove kvm specific hostid test

--- spec/ubuntu/hostid_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'spec_helper'
-
-# Ensure the hostid value is unique. See IMAGE-462.
-# https://bugs.launchpad.net/orinda/+bug/1284713
-describe command('hostid') do
-  its(:stdout) { should_not match "00000000" }
-end
-
