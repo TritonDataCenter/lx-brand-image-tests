b8f26007e98d6e96249cf16a836fbbe6364d8506
Christopher Horrell <christopher@joyent.com>
2015-03-15 21:37:55 -0400

    No need to test for grub settings on lx-brand

--- spec/ubuntu/grub_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe file('/etc/default/grub.d/50-cloudimg-settings.cfg') do
-	it { should be_file }
-	it { should contain "GRUB_CMDLINE_LINUX_DEFAULT=\"tsc=reliable earlyprintk\"" }
-  it { should contain "GRUB_TIMEOUT=5" }
-end
-
-
