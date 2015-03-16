86f527f3bc4916efecc1e18a21d0ef9ce816d5b4
Christopher Horrell <christopher@joyent.com>
2015-03-15 21:37:17 -0400

    Move shared tests to common
    guest tools should be the same for all images

--- spec/ubuntu/login_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'spec_helper'
-
-# Make sure ssh login is via ssh key only.
-describe file('/etc/ssh/sshd_config') do
-  it { should be_file }
-  it { should contain "PasswordAuthentication no" }
-end
