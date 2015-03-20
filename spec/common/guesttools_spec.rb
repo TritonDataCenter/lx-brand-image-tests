9f0ee8dbf8c016f85a041901fd792e50ae1cd8bc
Christopher Horrell <christopher@joyent.com>
2015-03-19 22:42:52 -0400

    mata-* tools are symlinks to native

--- spec/common/guesttools_spec.rb
+++ spec/common/guesttools_spec.rb
@@ -1,21 +1,21 @@
 require 'spec_helper'
 
 describe file('/usr/sbin/mdata-get') do
-  it { should be_file }
-	it { should be_mode 755 }
+  it { should be_symlink }
+	it { should be_linked_to '/native/usr/sbin/mdata-get' }
 end
 
 describe file('/usr/sbin/mdata-list') do
-  it { should be_file }
-  it { should be_mode 755 }
+  it { should be_symlink }
+  it { should be_linked_to '/native/usr/sbin/mdata-list' }
 end
 
 describe file('/usr/sbin/mdata-put') do
-  it { should be_file }
-  it { should be_mode 755 }
+  it { should be_symlink }
+  it { should be_linked_to '/native/usr/sbin/mdata-put' }
 end
 
 describe file('/usr/sbin/mdata-delete') do
-  it { should be_file }
-  it { should be_mode 755 }
+  it { should be_symlink }
+  it { should be_linked_to '/native/usr/sbin/mdata-delete' }
 end
