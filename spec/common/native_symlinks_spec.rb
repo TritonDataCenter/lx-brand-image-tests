e7a4c8e8d5ed1b680be4b4dc0b16bb9964e2a17f
Christopher Horrell <christopher@joyent.com>
2015-03-26 10:28:42 -0400

    Test for additional symlinks

--- spec/common/native_symlinks_spec.rb
+++ spec/common/native_symlinks_spec.rb
@@ -1,5 +1,17 @@
 require 'spec_helper'
 
+# Symlinks for bnaries in /native/usr/sbin/
+
+describe file('/usr/sbin/cpustat') do
+  it { should be_symlink }
+	it { should be_linked_to '/native/usr/sbin/cpustat' }
+end
+  
+describe file('/usr/sbin/dtrace') do
+  it { should be_symlink }
+	it { should be_linked_to '/native/usr/sbin/dtrace' }
+end
+  
 describe file('/usr/sbin/mdata-get') do
   it { should be_symlink }
 	it { should be_linked_to '/native/usr/sbin/mdata-get' }
@@ -19,3 +31,9 @@ describe file('/usr/sbin/mdata-delete') do
   it { should be_symlink }
   it { should be_linked_to '/native/usr/sbin/mdata-delete' }
 end
+
+describe file('/usr/sbin/plockstat') do
+  it { should be_symlink }
+  it { should be_linked_to '/native/usr/sbin/plockstat' }
+end
+
