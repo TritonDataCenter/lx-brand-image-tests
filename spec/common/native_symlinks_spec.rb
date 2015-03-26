dd2a8af5b8282925846974de6f848e133873cf53
Christopher Horrell <christopher@joyent.com>
2015-03-26 10:37:12 -0400

    Added tests for /native/usr/bin symlinks

--- spec/common/native_symlinks_spec.rb
+++ spec/common/native_symlinks_spec.rb
@@ -1,5 +1,82 @@
 require 'spec_helper'
 
+# Test for symlinks for binaries in /native/usr/bin/
+
+describe file('/usr/bin/mdb') do
+  it { should be_symlink }
+	it { should be_linked_to '/native/usr/bin/mdb' }
+end
+
+describe file('/usr/bin/pcred') do
+  it { should be_symlink }
+	it { should be_linked_to '/native/usr/bin/pcred' }
+end
+
+describe file('/usr/bin/pfiles') do
+  it { should be_symlink }
+	it { should be_linked_to '/native/usr/bin/pfiles' }
+end
+
+describe file('/usr/bin/pflag') do
+  it { should be_symlink }
+	it { should be_linked_to '/native/usr/bin/pflag' }
+end
+
+describe file('/usr/bin/pldd') do
+  it { should be_symlink }
+	it { should be_linked_to '/native/usr/bin/pldd' }
+end
+
+describe file('/usr/bin/prstat') do
+  it { should be_symlink }
+	it { should be_linked_to '/native/usr/bin/prstat' }
+end
+
+describe file('/usr/bin/prun') do
+  it { should be_symlink }
+	it { should be_linked_to '/native/usr/bin/prun' }
+end
+
+describe file('/usr/bin/psig') do
+  it { should be_symlink }
+	it { should be_linked_to '/native/usr/bin/psig' }
+end
+
+describe file('/usr/bin/pstack') do
+  it { should be_symlink }
+	it { should be_linked_to '/native/usr/bin/pstack' }
+end
+
+describe file('/usr/bin/pstop') do
+  it { should be_symlink }
+	it { should be_linked_to '/native/usr/bin/pstop' }
+end
+
+describe file('/usr/bin/ptime') do
+  it { should be_symlink }
+	it { should be_linked_to '/native/usr/bin/ptime' }
+end
+
+describe file('/usr/bin/pwait') do
+  it { should be_symlink }
+	it { should be_linked_to '/native/usr/bin/pwait' }
+end
+
+describe file('/usr/bin/pwdx') do
+  it { should be_symlink }
+	it { should be_linked_to '/native/usr/bin/pwdx' }
+end
+
+describe file('/usr/bin/truss') do
+  it { should be_symlink }
+	it { should be_linked_to '/native/usr/bin/truss' }
+end
+
+describe file('/usr/bin/kstat') do
+  it { should be_symlink }
+	it { should be_linked_to '/native/usr/bin/kstat' }
+end
+
 # Test for symlinks for binaries in /native/usr/sbin/
 
 describe file('/usr/sbin/cpustat') do
