920ea94f00841f90954bc6a0fc062ef512f33c40
Christopher Horrell <christopher@joyent.com>
2015-03-27 13:17:20 -0400

    Swap content for locale and debs tests

--- spec/ubuntu/locale_spec.rb
+++ spec/ubuntu/locale_spec.rb
@@ -1,18 +1,6 @@
 require 'spec_helper'
 
-describe file('/etc/apt/sources.list') do
+describe file('/etc/default/locale') do
 	it { should be_file }
-	it { should contain "deb http://archive.ubuntu.com/ubuntu trusty main" }
-  it { should contain "deb-src http://archive.ubuntu.com/ubuntu trusty main" }
-  it { should contain "deb http://archive.ubuntu.com/ubuntu trusty-updates main" }
-  it { should contain "deb-src http://archive.ubuntu.com/ubuntu trusty-updates main" }
-  it { should contain "deb http://security.ubuntu.com/ubuntu trusty-security main" }
-  it { should contain "deb-src http://security.ubuntu.com/ubuntu trusty-security main" }
-  it { should contain "deb http://archive.ubuntu.com/ubuntu trusty universe" }
-  it { should contain "deb-src http://archive.ubuntu.com/ubuntu trusty universe" }
-  it { should contain "deb http://archive.ubuntu.com/ubuntu trusty-updates universe" }
-  it { should contain "deb-src http://archive.ubuntu.com/ubuntu trusty-updates universe" }
-  it { should contain "deb http://security.ubuntu.com/ubuntu trusty-security universe" }
-  it { should contain "deb-src http://security.ubuntu.com/ubuntu trusty-security universe" }
+	it { should contain "LANG=\"en_US.UTF-8\"" }
 end
-
