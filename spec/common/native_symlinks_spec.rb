bc641631618f4a407d6465044a7cd749881e2d37
Christopher Horrell <christopher@horrell.ca>
2015-04-09 09:33:48 -0400

    spaces not tabs

--- spec/common/native_symlinks_spec.rb
+++ spec/common/native_symlinks_spec.rb
@@ -4,97 +4,97 @@ require 'spec_helper'
 
 describe file('/usr/bin/mdb') do
   it { should be_symlink }
-	it { should be_linked_to '/native/usr/bin/mdb' }
+  it { should be_linked_to '/native/usr/bin/mdb' }
 end
 
 describe file('/usr/bin/pcred') do
   it { should be_symlink }
-	it { should be_linked_to '/native/usr/bin/pcred' }
+  it { should be_linked_to '/native/usr/bin/pcred' }
 end
 
 describe file('/usr/bin/pfiles') do
   it { should be_symlink }
-	it { should be_linked_to '/native/usr/bin/pfiles' }
+  it { should be_linked_to '/native/usr/bin/pfiles' }
 end
 
 describe file('/usr/bin/pflags') do
   it { should be_symlink }
-	it { should be_linked_to '/native/usr/bin/pflags' }
+  it { should be_linked_to '/native/usr/bin/pflags' }
 end
 
 # Skip this test for Ubuntu. It has a native version of pldd
 if !property[:name].include? "Ubuntu"
   describe file('/usr/bin/pldd') do
     it { should be_symlink }
-  	it { should be_linked_to '/native/usr/bin/pldd' }
+    it { should be_linked_to '/native/usr/bin/pldd' }
   end
 end
 
 describe file('/usr/bin/prstat') do
   it { should be_symlink }
-	it { should be_linked_to '/native/usr/bin/prstat' }
+  it { should be_linked_to '/native/usr/bin/prstat' }
 end
 
 describe file('/usr/bin/prun') do
   it { should be_symlink }
-	it { should be_linked_to '/native/usr/bin/prun' }
+  it { should be_linked_to '/native/usr/bin/prun' }
 end
 
 describe file('/usr/bin/psig') do
   it { should be_symlink }
-	it { should be_linked_to '/native/usr/bin/psig' }
+  it { should be_linked_to '/native/usr/bin/psig' }
 end
 
 describe file('/usr/bin/pstack') do
   it { should be_symlink }
-	it { should be_linked_to '/native/usr/bin/pstack' }
+  it { should be_linked_to '/native/usr/bin/pstack' }
 end
 
 describe file('/usr/bin/pstop') do
   it { should be_symlink }
-	it { should be_linked_to '/native/usr/bin/pstop' }
+  it { should be_linked_to '/native/usr/bin/pstop' }
 end
 
 describe file('/usr/bin/ptime') do
   it { should be_symlink }
-	it { should be_linked_to '/native/usr/bin/ptime' }
+  it { should be_linked_to '/native/usr/bin/ptime' }
 end
 
 describe file('/usr/bin/pwait') do
   it { should be_symlink }
-	it { should be_linked_to '/native/usr/bin/pwait' }
+  it { should be_linked_to '/native/usr/bin/pwait' }
 end
 
 describe file('/usr/bin/truss') do
   it { should be_symlink }
-	it { should be_linked_to '/native/usr/bin/truss' }
+  it { should be_linked_to '/native/usr/bin/truss' }
 end
 
 describe file('/usr/bin/kstat') do
   it { should be_symlink }
-	it { should be_linked_to '/native/usr/bin/kstat' }
+  it { should be_linked_to '/native/usr/bin/kstat' }
 end
 
 describe file('/usr/bin/zonename') do
   it { should be_symlink }
-	it { should be_linked_to '/native/usr/bin/zonename' }
+  it { should be_linked_to '/native/usr/bin/zonename' }
 end
 
 # Test for symlinks for binaries in /native/usr/sbin/
 
 describe file('/usr/sbin/cpustat') do
   it { should be_symlink }
-	it { should be_linked_to '/native/usr/sbin/cpustat' }
+  it { should be_linked_to '/native/usr/sbin/cpustat' }
 end
   
 describe file('/usr/sbin/dtrace') do
   it { should be_symlink }
-	it { should be_linked_to '/native/usr/sbin/dtrace' }
+  it { should be_linked_to '/native/usr/sbin/dtrace' }
 end
   
 describe file('/usr/sbin/mdata-get') do
   it { should be_symlink }
-	it { should be_linked_to '/native/usr/sbin/mdata-get' }
+  it { should be_linked_to '/native/usr/sbin/mdata-get' }
 end
 
 describe file('/usr/sbin/mdata-list') do
