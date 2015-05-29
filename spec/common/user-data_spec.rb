8316d908eb60a7bbb3e088d08411a91a0a4640ea
Christopher Horrell <christopher@joyent.com>
2015-05-29 09:12:27 -0400

    Better test coverage for user-data and user-script
    These test require a test VM to have the expected user-data and
    user-script created. See tests and comments for details.

---
+++ spec/common/user-data_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+# This test requires that you are provisioning a VM with user-data
+# string "This is user-data!"
+describe file('/var/db/mdata-user-data') do
+  it { should be_file }
+  it { should_not be_executable }
+  it { should be_owned_by 'root' }
+  its(:content) { should match /This is user-data!/ }
+end
