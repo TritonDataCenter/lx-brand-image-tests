17abe326da66e57878c8e75377bbed187c9a99ee
Christopher Horrell <christopher@joyent.com>
2015-06-12 23:26:52 -0400

    Fix resolvconf test
    the tail file can be auto generated at provision

--- spec/ubuntu/resolvconf_spec.rb
+++ spec/ubuntu/resolvconf_spec.rb
@@ -1,7 +1,6 @@
 require 'spec_helper'
 
 describe command('ls /etc/resolvconf/resolv.conf.d/') do
-  its(:stdout) { should_not contain('tail') }
   its(:stdout) { should_not contain('original') }
 end
 
