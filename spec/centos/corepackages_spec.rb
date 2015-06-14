0f523a5f8f3fcb506fbc9b0cf28d866719ce062c
Christopher Horrell <christopher@joyent.com>
2015-06-14 12:23:18 -0400

    Make sure cron, sudo which and vim-minimal are installed

--- spec/centos/corepackages_spec.rb
+++ spec/centos/corepackages_spec.rb
@@ -1,10 +1,15 @@
 require 'spec_helper'
 
+describe package('cron') do
+  it { should be_installed }
+end
+
 describe package('iputils') do
   it { should be_installed }
 end
 
-describe package('curl') do
+# The @core installation does not include 'man'...
+describe package('man') do
   it { should be_installed }
 end
 
@@ -12,7 +17,14 @@ describe package('openssh-clients') do
   it { should be_installed }
 end
 
-# The @core installation does not include 'man'...
-describe package('man') do
+describe package('sudo') do
+  it { should be_installed }
+end
+
+describe package('vim-minimal') do
+  it { should be_installed }
+end
+
+describe package('which') do
   it { should be_installed }
 end
