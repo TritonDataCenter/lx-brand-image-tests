3f60c621f7446db7cd8688b0c09d0a1966e77593
Christopher Horrell <christopher@joyent.com>
2015-06-13 21:15:23 -0400

    Updated test for corepackages
    Ensure cron, inetutils-ping iproute (debian), net-tools and rsyslog are
    installed.
    
    No longer test for locales

--- spec/debian/corepackages_spec.rb
+++ spec/debian/corepackages_spec.rb
@@ -1,5 +1,9 @@
 require 'spec_helper'
 
+describe package('cron') do
+  it { should be_installed }
+end
+
 describe package('openssh-server') do
   it { should be_installed }
 end
@@ -8,3 +12,18 @@ describe package('sudo') do
   it { should be_installed }
 end
 
+describe package('inetutils-ping') do
+  it { should be_installed }
+end
+
+describe package('iproute') do
+  it { should be_installed }
+end
+
+describe package('net-tools') do
+  it { should be_installed }
+end
+
+describe package('rsyslog') do
+  it { should be_installed }
+end
--- spec/ubuntu/corepackages_spec.rb
+++ spec/ubuntu/corepackages_spec.rb
@@ -1,9 +1,21 @@
 require 'spec_helper'
 
+describe package('cron') do
+  it { should be_installed }
+end
+
 describe package('openssh-server') do
   it { should be_installed }
 end
 
-describe package('locales') do
+describe package('iputils-ping') do
+  it { should be_installed }
+end
+
+describe package('net-tools') do
+  it { should be_installed }
+end
+
+describe package('rsyslog') do
   it { should be_installed }
 end
