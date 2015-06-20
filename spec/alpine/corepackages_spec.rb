a8f3aa4d4e2baefd26711a63152f52fa7bf0a24e
Christopher Horrell <christopher@joyent.com>
2015-06-20 12:51:04 -0400

    vim, curl, wget and less are no longer preinstalled in the Alpine Linux image

--- spec/alpine/corepackages_spec.rb
+++ spec/alpine/corepackages_spec.rb
@@ -7,19 +7,3 @@ end
 describe package('openssh') do
   it { should be_installed }
 end
-
-describe package('vim') do
-  it { should be_installed }
-end
-
-describe package('curl') do
-  it { should be_installed }
-end
-
-describe package('wget') do
-  it { should be_installed }
-end
-
-describe package('less') do
-  it { should be_installed }
-end
