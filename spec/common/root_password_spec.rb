814ea37828496c8714c8657fd773d0f875f6ef9f
Christopher Horrell <christopher@joyent.com>
2015-07-16 15:56:39 -0400

    Better test to ensure not root password is set
    Now testing for '*' or '' via regex

--- spec/common/root_password_spec.rb
+++ spec/common/root_password_spec.rb
@@ -2,17 +2,11 @@ require 'spec_helper'
 
 # No root password should be set
 
-if property[:name].include? "Alpine"
-  # An alternate test for Alpine Linux. The value will be ''
-  describe command("cat /etc/shadow | grep root | awk -F':' '{print $2;}'") do
-    its(:stdout) { should eq "\n" }
-  end
-else
-  # Test for "*" for everything else
-  describe command("cat /etc/shadow | grep root | awk -F':' '{print $2;}'") do
-    its(:stdout) { should eq "*\n" }
-  end
+# Test for "*/n" or "/n" 
+describe command("cat /etc/shadow | grep root | awk -F':' '{print $2;}'") do
+  its(:stdout) { should match /\*?\n/ }
 end
+
 
 
 
