267deff805fbfc3f53800fe60c600a40e89bbc13
Christopher Horrell <christopher@joyent.com>
2015-07-16 22:11:14 -0400

    formatting

--- spec/common/install_chef_spec.rb
+++ spec/common/install_chef_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 # Make sure Chef installs
 
 if ! property[:name].include? "Alpine"
-describe command('curl -kL https://www.chef.io/chef/install.sh | bash') do
-  its(:exit_status) { should eq 0 }
-end
+  describe command('curl -kL https://www.chef.io/chef/install.sh | bash') do
+    its(:exit_status) { should eq 0 }
+  end
 end
