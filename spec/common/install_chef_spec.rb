0c93dcc1ca6e2ecdb57a025fe7aeb85b66214160
Christopher Horrell <christopher@joyent.com>
2015-07-16 22:11:02 -0400

    Don't do a test install of Chef on Alpine, it's not supported

--- spec/common/install_chef_spec.rb
+++ spec/common/install_chef_spec.rb
@@ -2,7 +2,8 @@ require 'spec_helper'
 
 # Make sure Chef installs
 
+if ! property[:name].include? "Alpine"
 describe command('curl -kL https://www.chef.io/chef/install.sh | bash') do
   its(:exit_status) { should eq 0 }
 end
-
+end
