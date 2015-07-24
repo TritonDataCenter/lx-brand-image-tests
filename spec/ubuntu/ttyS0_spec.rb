a9aca192e8e070ae9b49f7057b33fc92938d5093
Christopher Horrell <christopher@joyent.com>
2015-07-24 15:25:41 -0400

    improved test for /etc/init/ttyS0.conf

--- spec/ubuntu/ttyS0_spec.rb
+++ spec/ubuntu/ttyS0_spec.rb
@@ -2,5 +2,6 @@ require 'spec_helper'
 
 describe file('/etc/init/ttyS0.conf') do
 	it { should be_file }
+  its(:sha256sum) { should eq '539050f264117743450c0fbbca4c7efe632a054d0e279fd1fa879b2ba6d1d589' }
 end
 
