bcb7f64250a60c42b444335d7da56e0d132ae987
Christopher Horrell <christopher@joyent.com>
2015-08-13 10:13:50 -0400

    Use double quotes and add missing escapes

--- spec/common/shellshock_spec.rb
+++ spec/common/shellshock_spec.rb
@@ -3,12 +3,12 @@ require 'spec_helper'
 # Test to ensure bash isn't susceptible to Shellshock (CVE-2014-6271)
 
 # CVE-2014-6271
-describe command('env x='() { :;}; echo vulnerable' bash -c "echo this is a test"') do
+describe command("env x='() { :;}; echo vulnerable' bash -c \"echo this is a test\"") do
   its(:stderr) { should_not match /vulnerable/ }
 end
 
 # CVE-2014-7169
-describe command('env X='() { (a)=>\' bash -c "echo date"') do
+describe command("env X='() { (a)=>\' bash -c \"echo date\"") do
   its(:stderr) { should match /date/ }
 end
 
