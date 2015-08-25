b88901715ac555c77676663903e52f3434eb06fe
Christopher Horrell <christopher@joyent.com>
2015-08-24 17:43:45 -0700

    Make sure vim is installed

--- spec/alpine/corepackages_spec.rb
+++ spec/alpine/corepackages_spec.rb
@@ -12,6 +12,10 @@ describe package('openssh') do
   it { should be_installed }
 end
 
+describe package('vim') do
+  it { should be_installed }
+end
+
 describe package('wget') do
   it { should be_installed }
 end
