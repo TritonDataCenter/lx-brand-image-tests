104e5bea183a26a21923a01e0eda2408c9011a6c
Christopher Horrell <christopher@joyent.com>
2015-09-30 17:07:16 -0400

    vim package is called vim-enhanced in CentOS

--- spec/common/packages_spec.rb
+++ spec/common/packages_spec.rb
@@ -8,8 +8,16 @@ describe package('less') do
   it { should be_installed }
 end
 
-describe package('vim') do
-  it { should be_installed }
+# On CentOS the package is called vim-enhanced
+
+if property[:name].include? "CentOS"
+  describe package('vim-enhanced') do
+    it { should be_installed }
+  end
+else
+  describe package('vim') do
+    it { should be_installed }
+  end
 end
 
 describe package('wget') do
