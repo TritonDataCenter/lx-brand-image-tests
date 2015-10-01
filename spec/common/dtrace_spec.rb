70b93ff59bb5da594a9f567d65a4b20a4c6b1f38
Christopher Horrell <christopher@joyent.com>
2015-10-01 11:50:39 -0400

    Use full path to dtrace
    The guest tools no longer add symlinks to /native binaries except
    mdata- tools

--- spec/common/dtrace_spec.rb
+++ spec/common/dtrace_spec.rb
@@ -3,25 +3,25 @@ require 'spec_helper'
 # Test DTrace commmands
 
 # Print version
-describe command('dtrace -V') do
+describe command('/native/usr/sbin/dtrace -V') do
   its(:exit_status) { should eq 0 }
   its(:stdout) { should contain('dtrace: Sun D ') }
 end
 
 # List all probes
-describe command('dtrace -l') do
+describe command('/native/usr/sbin/dtrace -l') do
   its(:exit_status) { should eq 0 }
   its(:stdout) { should contain('PROVIDER') }
 end
 
 # List all probes provided by the syscall provider
-describe command('dtrace -l -P syscall') do
+describe command('/native/usr/sbin/dtrace -l -P syscall') do
   its(:exit_status) { should eq 0 }
   its(:stdout) { should contain('PROVIDER') }
 end
 
 # Print all entry probes provided by the syscall provide
-describe command('dtrace -l -n syscall:::entry') do
+describe command('/native/usr/sbin/dtrace -l -n syscall:::entry') do
   its(:exit_status) { should eq 0 }
   its(:stdout) { should contain('PROVIDER') }
 end
