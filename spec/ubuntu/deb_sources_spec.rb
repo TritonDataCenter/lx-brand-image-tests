be75308202426c073bee664c3d07f20db97053b5
Christopher Horrell <christopher@joyent.com>
2015-11-05 11:19:49 -0500

    Make sure to test for the right release for deb_sources

--- spec/ubuntu/deb_sources_spec.rb
+++ spec/ubuntu/deb_sources_spec.rb
@@ -1,18 +1,41 @@
 require 'spec_helper'
 
-describe file('/etc/apt/sources.list') do
-	it { should be_file }
-	it { should contain "deb http://archive.ubuntu.com/ubuntu trusty main" }
-  it { should contain "deb-src http://archive.ubuntu.com/ubuntu trusty main" }
-  it { should contain "deb http://archive.ubuntu.com/ubuntu trusty-updates main" }
-  it { should contain "deb-src http://archive.ubuntu.com/ubuntu trusty-updates main" }
-  it { should contain "deb http://security.ubuntu.com/ubuntu trusty-security main" }
-  it { should contain "deb-src http://security.ubuntu.com/ubuntu trusty-security main" }
-  it { should contain "deb http://archive.ubuntu.com/ubuntu trusty universe" }
-  it { should contain "deb-src http://archive.ubuntu.com/ubuntu trusty universe" }
-  it { should contain "deb http://archive.ubuntu.com/ubuntu trusty-updates universe" }
-  it { should contain "deb-src http://archive.ubuntu.com/ubuntu trusty-updates universe" }
-  it { should contain "deb http://security.ubuntu.com/ubuntu trusty-security universe" }
-  it { should contain "deb-src http://security.ubuntu.com/ubuntu trusty-security universe" }
+
+if property[:name].include? "Ubuntu 14.04"
+  describe file('/etc/apt/sources.list') do
+  	it { should be_file }
+  	it { should contain "deb http://archive.ubuntu.com/ubuntu trusty main" }
+    it { should contain "deb-src http://archive.ubuntu.com/ubuntu trusty main" }
+    it { should contain "deb http://archive.ubuntu.com/ubuntu trusty-updates main" }
+    it { should contain "deb-src http://archive.ubuntu.com/ubuntu trusty-updates main" }
+    it { should contain "deb http://security.ubuntu.com/ubuntu trusty-security main" }
+    it { should contain "deb-src http://security.ubuntu.com/ubuntu trusty-security main" }
+    it { should contain "deb http://archive.ubuntu.com/ubuntu trusty universe" }
+    it { should contain "deb-src http://archive.ubuntu.com/ubuntu trusty universe" }
+    it { should contain "deb http://archive.ubuntu.com/ubuntu trusty-updates universe" }
+    it { should contain "deb-src http://archive.ubuntu.com/ubuntu trusty-updates universe" }
+    it { should contain "deb http://security.ubuntu.com/ubuntu trusty-security universe" }
+    it { should contain "deb-src http://security.ubuntu.com/ubuntu trusty-security universe" }
+  end
 end
+
+if property[:name].include? "Ubuntu 15.04"
+  describe file('/etc/apt/sources.list') do
+  	it { should be_file }
+  	it { should contain "deb http://archive.ubuntu.com/ubuntu vivid main" }
+    it { should contain "deb-src http://archive.ubuntu.com/ubuntu vivid main" }
+    it { should contain "deb http://archive.ubuntu.com/ubuntu vivid-updates main" }
+    it { should contain "deb-src http://archive.ubuntu.com/ubuntu vivid-updates main" }
+    it { should contain "deb http://security.ubuntu.com/ubuntu vivid-security main" }
+    it { should contain "deb-src http://security.ubuntu.com/ubuntu vivid-security main" }
+    it { should contain "deb http://archive.ubuntu.com/ubuntu vivid universe" }
+    it { should contain "deb-src http://archive.ubuntu.com/ubuntu vivid universe" }
+    it { should contain "deb http://archive.ubuntu.com/ubuntu vivid-updates universe" }
+    it { should contain "deb-src http://archive.ubuntu.com/ubuntu vivid-updates universe" }
+    it { should contain "deb http://security.ubuntu.com/ubuntu vivid-security universe" }
+    it { should contain "deb-src http://security.ubuntu.com/ubuntu vivid-security universe" }
+  end
+end
+
+
 
