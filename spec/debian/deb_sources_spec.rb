c599573fd61e272508fde9cab717225be9bda2b5
Christopher Horrell <christopher@joyent.com>
2015-11-09 15:55:36 -0500

    Make sure we test for the right release in sources.list

--- spec/debian/deb_sources_spec.rb
+++ spec/debian/deb_sources_spec.rb
@@ -1,11 +1,25 @@
 require 'spec_helper'
 
-describe file('/etc/apt/sources.list') do
-  it { should be_file }
-  it { should contain "deb http://httpredir.debian.org/debian wheezy main" }
-  it { should contain "deb-src http://httpredir.debian.org/debian wheezy main" }
-  it { should contain "deb http://httpredir.debian.org/debian wheezy-updates main" }
-  it { should contain "deb-src http://httpredir.debian.org/debian wheezy-updates main" }
-  it { should contain "deb http://security.debian.org/ wheezy/updates  main" }
-  it { should contain "deb-src http://security.debian.org/ wheezy/updates  main" }
+if property[:name].include? "Debian 8"
+  describe file('/etc/apt/sources.list') do
+    it { should be_file }
+    it { should contain "deb http://httpredir.debian.org/debian jessie main" }
+    it { should contain "deb-src http://httpredir.debian.org/debian jessie main" }
+    it { should contain "deb http://httpredir.debian.org/debian jessie-updates main" }
+    it { should contain "deb-src http://httpredir.debian.org/debian jessie-updates main" }
+    it { should contain "deb http://security.debian.org/ jessie/updates  main" }
+    it { should contain "deb-src http://security.debian.org/ jessie/updates  main" }
+  end
+end
+
+if property[:name].include? "Debian 7"
+  describe file('/etc/apt/sources.list') do
+    it { should be_file }
+    it { should contain "deb http://httpredir.debian.org/debian wheezy main" }
+    it { should contain "deb-src http://httpredir.debian.org/debian wheezy main" }
+    it { should contain "deb http://httpredir.debian.org/debian wheezy-updates main" }
+    it { should contain "deb-src http://httpredir.debian.org/debian wheezy-updates main" }
+    it { should contain "deb http://security.debian.org/ wheezy/updates  main" }
+    it { should contain "deb-src http://security.debian.org/ wheezy/updates  main" }
+  end
 end
