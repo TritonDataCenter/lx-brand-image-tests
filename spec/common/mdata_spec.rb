aa151833618882a40f5e3b6dee4510702d1fa5d2
Christopher Horrell <christopher@horrell.ca>
2015-11-19 15:27:58 -0500

    Test for mdata-* tools symlinks

--- spec/common/mdata_spec.rb
+++ spec/common/mdata_spec.rb
@@ -1,5 +1,35 @@
 require 'spec_helper'
 
+# Test for mdata-* tools symlinks
+
+describe file('/usr/sbin/mdata-get') do
+  it { should be_file }
+  it { should be_symlink }
+  it { should be_linked_to '/native/usr/sbin/mdata-get' }
+  it { should be_mode 755 }
+end
+
+describe file('/usr/sbin/mdata-list') do
+  it { should be_file }
+  it { should be_symlink }
+  it { should be_linked_to '/native/usr/sbin/mdata-list' }
+  it { should be_mode 755 }
+end
+
+describe file('/usr/sbin/mdata-put') do
+  it { should be_file }
+  it { should be_symlink }
+  it { should be_linked_to '/native/usr/sbin/mdata-put' }
+  it { should be_mode 755 }
+end
+
+describe file('/usr/sbin/mdata-delete') do
+  it { should be_file }
+  it { should be_symlink }
+  it { should be_linked_to '/native/usr/sbin/mdata-delete' }
+  it { should be_mode 755 }
+end
+
 # Test mdata-* commands
 
 describe command('mdata-put test test') do
