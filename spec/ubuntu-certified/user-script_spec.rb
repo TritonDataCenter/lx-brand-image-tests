3ca66b57a9680fd0513fadb57f32edde89303fda
Christopher Horrell <christopher@horrell.ca>
2015-11-23 16:35:42 -0500

    User-script and user-data

---
+++ spec/ubuntu-certified/user-script_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+# This test requires that you are provisioning an instance with user-script 
+# that contains:
+#
+# #!/bin/sh
+# echo "testing user-script" >> /var/tmp/test
+describe file('/var/db/user-script') do
+	it { should be_file }
+  it { should be_owned_by 'root' }
+  it { should be_executable }
+  its(:content) { should match /#!\/bin\/sh/ }
+  its(:content) { should match /echo "testing user-script" >> \/var\/tmp\/test/ }
+end
+
+# The test user-script (see above) should create the file /var/tmp/test
+# with the contents "testing user-script".
+describe file('/var/tmp/test') do
+  it { should be_file }
+  its(:content) { should match /testing user-script/ }
+end
