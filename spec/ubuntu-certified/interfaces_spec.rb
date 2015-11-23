c1d4567163c9c34acdc4fb8c0e16136cee836137
Christopher Horrell <christopher@horrell.ca>
2015-11-23 16:22:50 -0500

    test interfaces

---
+++ spec/ubuntu-certified/interfaces_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+# This test requires a VM to be provision with two IPs, preferably one public
+# and one private.
+
+# Test to ensure the VM has two interfaces, eth0 and eth1
+
+# eth0
+describe interface('eth0') do
+  it { should exist }
+end
+
+describe command('ifconfig eth0 | grep UP | wc -l') do
+  its(:exit_status) { should eq 0 }
+  its(:stdout) { should match /1/ }
+end
+
+# eth1
+describe interface('eth1') do
+  it { should exist }
+end
+
+describe command('ifconfig eth1 | grep UP | wc -l') do
+  its(:exit_status) { should eq 0 }
+  its(:stdout) { should match /1/ }
+end
