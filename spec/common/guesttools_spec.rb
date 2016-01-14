18462f16f23b7a9ad296a11449e072a722680836
Christopher Horrell <christopher@horrell.ca>
2016-01-13 22:07:47 -0500

    remove extra end

--- spec/common/guesttools_spec.rb
+++ spec/common/guesttools_spec.rb
@@ -57,8 +57,6 @@ describe file('/lib/smartdc/set-provision-state') do
   it { should be_executable }
 end
 
-end
-
 describe file('/var/log/triton.log') do
   it { should be_file }
   it { should be_owned_by 'root' }
