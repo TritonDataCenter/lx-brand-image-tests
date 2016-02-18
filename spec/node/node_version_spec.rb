f433ddd14638cb9a375d1f3a62ee892ae665b43f
Christopher Horrell <christopher@horrell.ca>
2016-02-18 16:30:15 -0500

    Fix node_version tests

--- spec/node/node_version_spec.rb
+++ spec/node/node_version_spec.rb
@@ -1,15 +1,15 @@
 require 'spec_helper'
 
 # Node 12.x
-if property[:name] =~ /nodejs-0.12/
+if property[:name] =~ /v0.12./
   describe command('node -v') do
-    its(:stdout) { should_not contain('v0.12.10') }
+    its(:stdout) { should contain('v0.12.10') }
   end
 end
 
 # Node 4.x
-if property[:name] =~ /nodejs-4/
+if property[:name] =~ /v4./
   describe command('node -v') do
-    its(:stdout) { should_not contain('v4.3.1') }
+    its(:stdout) { should contain('v4.3.1') }
   end
 end
