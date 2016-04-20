7e1815fa6435807c97c994197d6f392bf4084572
Christopher Horrell <christopher@horrell.ca>
2016-04-20 17:14:51 -0400

    Update node version tests

--- spec/node/node_version_spec.rb
+++ spec/node/node_version_spec.rb
@@ -3,13 +3,13 @@ require 'spec_helper'
 # Node 12.x
 if property[:name] =~ /v0.12./
   describe command('node -v') do
-    its(:stdout) { should contain('v0.12.10') }
+    its(:stdout) { should contain('v0.12.13') }
   end
 end
 
 # Node 4.x
 if property[:name] =~ /v4./
   describe command('node -v') do
-    its(:stdout) { should contain('v4.3.1') }
+    its(:stdout) { should contain('v4.4.3') }
   end
 end
