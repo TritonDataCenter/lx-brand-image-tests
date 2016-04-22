2f9d9413f488bb6911f65b1c997b7b03cfc81878
Christopher Horrell <christopher@horrell.ca>
2016-04-22 09:58:42 -0400

    Test for apt pinning of udev in Ubuntu 16.xx and 17.xx
    Adding 17.XX to be preemptive.

--- spec/ubuntu/apt-get_spec.rb
+++ spec/ubuntu/apt-get_spec.rb
@@ -16,3 +16,22 @@ end
 describe command('apt-get -y remove apg') do
   its(:exit_status) { should eq 0 }
 end
+
+# Make sure we apt pin udev in Ubuntu 16.XX and 17.XX
+if property[:name] =~ /Ubuntu 16./
+  describe file('/etc/apt/preferences.d/udev') do
+    it { should be_file }
+    its(:content) { should match /Package: udev/ }
+    its(:content) { should match /Pin: release */ }
+    its(:content) { should match /Pin-Priority: -1/ }
+  end
+end
+
+if property[:name] =~ /Ubuntu 17./
+  describe file('/etc/apt/preferences.d/udev') do
+    it { should be_file }
+    its(:content) { should match /Package: udev/ }
+    its(:content) { should match /Pin: release */ }
+    its(:content) { should match /Pin-Priority: -1/ }
+  end
+end
