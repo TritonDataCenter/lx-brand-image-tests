704ae07e508c7d6025ca75b2d7163be63cec7b65
Christopher Horrell <christopher@joyent.com>
2016-05-23 19:03:51 -0700

    fix test for /etc/ssh/sshd_config

--- spec/image-config/login_spec.rb
+++ spec/image-config/login_spec.rb
@@ -3,5 +3,5 @@ require 'spec_helper'
 # Make sure ssh login is via ssh key only.
 describe file('/etc/ssh/sshd_config') do
   it { should be_file }
-  it { should contain "PasswordAuthentication no" }
+  its(:content) { should match /^PasswordAuthentication no/ }
 end
