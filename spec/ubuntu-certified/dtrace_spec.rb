cb2a3b92e1bb9aacdf3094bd36677f5c49a0f777
Derek Crudgington <derek@joyent.com>
2016-06-01 15:15:05 +0000

    Added more DTrace tests

--- spec/ubuntu-certified/dtrace_spec.rb
+++ spec/ubuntu-certified/dtrace_spec.rb
@@ -25,3 +25,24 @@ describe command('/native/usr/sbin/dtrace -l -n syscall:::entry') do
   its(:exit_status) { should eq 0 }
   its(:stdout) { should contain('PROVIDER') }
 end
+
+# Various DTrace one liners
+describe command('/native/usr/sbin/dtrace -n \'lx-syscall:::entry { @num[execname] = count(); } profile:::tick-1sec { exit(0); }\'') do
+  its(:exit_status) { should eq 0 }
+end
+
+describe command('/native/usr/sbin/dtrace -n \'lx-syscall:::entry { @num[pid, execname, probefunc] = count(); } profile:::tick-1sec { exit(0); }\'') do
+  its(:exit_status) { should eq 0 }
+end
+
+describe command('/native/usr/sbin/dtrace -n \'profile:::profile-1000hz { @Count[execname, pid] = lquantize(curlwpsinfo->pr_pri, 0, 170, 5); } profile:::tick-1sec { exit(0); }\'') do
+  its(:exit_status) { should eq 0 }
+end
+
+describe command('/native/usr/sbin/dtrace -n \'proc:::exec-success { trace(curpsinfo->pr_psargs); } profile:::tick-1sec { exit(0); }\'') do
+  its(:exit_status) { should eq 0 }
+end
+
+describe command('/native/usr/sbin/dtrace -n \'lx-syscall::open:entry { printf("%s %s", execname, copyinstr(arg0)); } profile:::tick-1sec { exit(0); }\'') do
+  its(:exit_status) { should eq 0 }
+end
