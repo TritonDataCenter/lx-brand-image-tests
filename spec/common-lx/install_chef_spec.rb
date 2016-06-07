c87f3fab4f09516a88e78fd96d65e75f1615529d
Christopher Horrell <christopher@joyent.com>
2016-06-07 14:35:01 -0400

    Better distro check for install_chef_spec

--- spec/common-lx/install_chef_spec.rb
+++ spec/common-lx/install_chef_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 # Make sure Chef installs
 
-if ! property[:name].include? "Alpine"
+if ! file('/etc/alpine-release').exists?
   describe command('curl -kL https://www.chef.io/chef/install.sh | bash') do
     its(:exit_status) { should eq 0 }
   end
