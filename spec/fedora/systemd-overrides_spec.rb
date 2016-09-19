2a93ad2399d23a66fde3b66d63aecfb65ad1d022
Christopher Horrell <christopher@joyent.com>
2016-09-19 15:30:53 -0400

    Remove unnecessary CentOS check in Fedora tests

--- spec/fedora/systemd-overrides_spec.rb
+++ spec/fedora/systemd-overrides_spec.rb
@@ -9,36 +9,32 @@ require 'spec_helper'
 #   - https://smartos.org/bugview/OS-5304
 
 describe "httpd service validation" do
-  # This file only exists in CentOS 7 and up which ensures we just test on
-  # versions with systemd
-  if file('/etc/os-release').exists?
-    describe "Systemd override for httpd shoud be valid" do
-      describe file('/etc/systemd/system/httpd.service.d/override.conf') do
-        it { should be_file }
-        its(:content) { should match /[Service]/ }
-        its(:content) { should match /PrivateTmp=no/ }
-      end
+  describe "Systemd override for httpd shoud be valid" do
+    describe file('/etc/systemd/system/httpd.service.d/override.conf') do
+      it { should be_file }
+      its(:content) { should match /[Service]/ }
+      its(:content) { should match /PrivateTmp=no/ }
     end
-  
-    describe "Install of httpd package should succeed" do
-      describe command('dnf -y install httpd') do
-        its(:exit_status) { should eq 0 }
-      end
-      describe command('systemctl restart httpd') do
-        its(:exit_status) { should eq 0 }
-      end
-    end
-  
-    describe service('httpd') do
-      it { should be_running }
-    end
-    
-    describe command('dnf -y erase httpd') do
+  end
+
+  describe "Install of httpd package should succeed" do
+    describe command('dnf -y install httpd') do
       its(:exit_status) { should eq 0 }
     end
-    
-    describe command('systemctl daemon-reload') do
+    describe command('systemctl restart httpd') do
       its(:exit_status) { should eq 0 }
     end
+  end
+
+  describe service('httpd') do
+    it { should be_running }
+  end
+
+  describe command('dnf -y erase httpd') do
+    its(:exit_status) { should eq 0 }
+  end
+
+  describe command('systemctl daemon-reload') do
+    its(:exit_status) { should eq 0 }
   end
 end
