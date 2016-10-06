26bc56fe6c67dd45698f89ab9242e622b7068642
Christopher Horrell <christopher@joyent.com>
2016-10-06 12:29:33 -0400

    Make ssh-keygen less noisy

--- run-image-config-tests.sh
+++ run-image-config-tests.sh
@@ -257,9 +257,9 @@ wait_for_IP $CUSTOM_INSTANCE_NAME
 test_image $CUSTOM_INSTANCE_NAME
 
 echo "Deleting instance and custom image"
-ssh-keygen -R $(triton inst ip $INSTACE_NAME)
+ssh-keygen -q -R $(triton inst ip $INSTACE_NAME)
 delete_instance $INSTACE_NAME
-ssh-keygen -R $(triton inst ip $CUSTOM_INSTANCE_NAME)
+ssh-keygen -q -R $(triton inst ip $CUSTOM_INSTANCE_NAME)
 delete_instance $CUSTOM_INSTANCE_NAME
 delete_image $CUSTOM_IMAGE_NAME
 cleanup
