776621f82073381b69764946b91b05172a6d3131
Christopher Horrell <christopher@joyent.com>
2016-10-11 10:34:19 -0400

    Update for latest versions of Node.js

--- spec/node/node_version_spec.rb
+++ spec/node/node_version_spec.rb
@@ -3,20 +3,20 @@ require 'spec_helper'
 # Node 12.x
 if property[:name] =~ /v0.12./
   describe command('node -v') do
-    its(:stdout) { should contain('v0.12.15') }
+    its(:stdout) { should contain('v0.12.16') }
   end
 end
 
 # Node 4.x
 if property[:name] =~ /v4./
   describe command('node -v') do
-    its(:stdout) { should contain('v4.4.6') }
+    its(:stdout) { should contain('v4.6.0') }
   end
 end
 
 # Node 6.x
 if property[:name] =~ /v6./
   describe command('node -v') do
-    its(:stdout) { should contain('v6.2.0') }
+    its(:stdout) { should contain('v6.7.0') }
   end
 end
