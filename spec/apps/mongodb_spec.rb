5b485701f6db76ebfe1cb11db081a092e02d7024
Derek Crudgington <derek@joyent.com>
2016-10-26 14:00:27 -0500

    Updated mongodb tests to distro check

--- spec/apps/mongodb_spec.rb
+++ spec/apps/mongodb_spec.rb
@@ -1,49 +1,52 @@
 require 'spec_helper'
 
+# Ubuntu and Debian tests
 # Ubuntu MongoDB install and version test
 
-describe command('grep -q Ubuntu /etc/product && apt-get install -y mongodb && mongo --eval="db.version();"') do
-  its(:exit_status) { should eq 0 }
-end
+if file('/etc/debian_version').exists?
+  describe command('apt-get install -y mongodb && mongo --eval="db.version();"') do
+    its(:exit_status) { should eq 0 }
+  end
 
-# Ubuntu MongoDB basic collection test
+  # MongoDB basic collection test
 
-describe command('grep -q Ubuntu /etc/product && mongo test3 --host localhost --eval="db.test3.save({country:\"England\",GroupName:\"D\"})" && mongo test3 --host localhost --eval="db.test3.find().forEach(printjson)"') do
-  its(:exit_status) { should eq 0 }
-end
+  describe command('mongo test3 --host localhost --eval="db.test3.save({country:\"England\",GroupName:\"D\"})" && mongo test3 --host localhost --eval="db.test3.find().forEach(printjson)"') do
+    its(:exit_status) { should eq 0 }
+  end
 
-# Ubuntu MongoDB server information test
+  # MongoDB server information test
 
-describe command('grep -q Ubuntu /etc/product && mongo --host localhost --eval="printjson(db.hostInfo())" && mongo --host localhost --eval="printjson(db.serverStatus())"') do
-  its(:exit_status) { should eq 0 }
-end
+  describe command('mongo --host localhost --eval="printjson(db.hostInfo())" && mongo --host localhost --eval="printjson(db.serverStatus())"') do
+    its(:exit_status) { should eq 0 }
+  end
 
-# Ubuntu MongoDB server mongostat test
+  # MongoDB server mongostat test
 
-describe command('grep -q Ubuntu /etc/product && mongostat -n 5') do
-  its(:exit_status) { should eq 0 }
-end
+  describe command('mongostat -n 5') do
+    its(:exit_status) { should eq 0 }
+  end
 
-# Ubuntu MongoDB server grab delicious file
+  # MongoDB server grab delicious file
 
-describe command('grep -q Ubuntu /etc/product && wget http://randomwalker.info/data/delicious/delicious-rss-1250k.gz') do
-  its(:exit_status) { should eq 0 }
-end
+  describe command('wget http://randomwalker.info/data/delicious/delicious-rss-1250k.gz') do
+    its(:exit_status) { should eq 0 }
+  end
 
-# Ubuntu MongoDB server gunzip delicious file
+  # MongoDB server gunzip delicious file
 
-describe command('grep -q Ubuntu /etc/product && gunzip delicious-rss-1250k.gz') do
-  its(:exit_status) { should eq 0 }
-end
+  describe command('gunzip -qf delicious-rss-1250k.gz') do
+    its(:exit_status) { should eq 0 }
+  end
 
-# Ubuntu MongoDB server mongoimport delicious file
+  # MongoDB server mongoimport delicious file
 
-describe command('grep -q Ubuntu /etc/product && mongoimport --db db1 --collection docs < delicious-rss-1250k') do
-  its(:exit_status) { should eq 0 }
-end
+  describe command('mongoimport --db db1 --collection docs < delicious-rss-1250k') do
+    its(:exit_status) { should eq 0 }
+  end
   
-# Ubuntu MongoDB server mongodump delicious db1
+  # MongoDB server mongodump delicious db1
 
-describe command('grep -q Ubuntu /etc/product && mongodump -o dumpfile1') do
-  its(:exit_status) { should eq 0 }
+  describe command('mongodump -o dumpfile1') do
+    its(:exit_status) { should eq 0 }
+  end
 end
