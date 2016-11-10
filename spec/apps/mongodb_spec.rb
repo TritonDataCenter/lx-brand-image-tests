edba3cc1136907091a2688927164d904544da09d
Derek Crudgington <derek@joyent.com>
2016-11-10 10:42:10 -0600

    Added cluster setup and testing to mongodb tests

--- spec/apps/mongodb_spec.rb
+++ spec/apps/mongodb_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 # Ubuntu and Debian tests
-# Ubuntu MongoDB install and version test
+# MongoDB install and version test
 if file('/etc/debian_version').exists?
   describe command('apt-get install -y mongodb && mongo --eval="db.version();"') do
     its(:exit_status) { should eq 0 }
@@ -39,6 +39,61 @@ if file('/etc/debian_version').exists?
   
   # MongoDB server mongodump delicious db1
   describe command('mongodump -o dumpfile1') do
+    its(:exit_status) { should eq 0 }
+  end
+  
+  # MongoDB cluster setup create data dirs
+  describe command('mkdir -p /var/lib/mongodb1 /var/lib/mongodb2 /var/lib/mongodb3') do
+    its(:exit_status) { should eq 0 }
+  end
+  
+  # MongoDB cluster setup node1
+  describe command('mongod --fork --port 27011 --dbpath /var/lib/mongodb1 --replSet rs1 --smallfiles --oplogSize 128 --logpath /var/log/mongodb/mongodb1.log') do
+    its(:exit_status) { should eq 0 }
+  end
+  
+  # MongoDB cluster setup node2
+  describe command('mongod --fork --port 27012 --dbpath /var/lib/mongodb2 --replSet rs1 --smallfiles --oplogSize 128 --logpath /var/log/mongodb/mongodb2.log') do
+    its(:exit_status) { should eq 0 }
+  end
+  
+  # MongoDB cluster setup node3
+  describe command('mongod --fork --port 27013 --dbpath /var/lib/mongodb3 --replSet rs1 --smallfiles --oplogSize 128 --logpath /var/log/mongodb/mongodb3.log') do
+    its(:exit_status) { should eq 0 }
+  end
+  
+  # MongoDB cluster setup set configuration
+  describe command('mongo --port 27011 --eval="rsconf = { _id: \"rs1\", members: [ { _id: 0, host: \"localhost:27011\" } ] }; rs.initiate( rsconf )"') do
+    its(:exit_status) { should eq 0 }
+  end
+  
+  # MongoDB cluster setup add node2 to configuration
+  describe command('sleep 25 && mongo --port 27011 --eval="rs.add(\"localhost:27012\")"') do
+    its(:exit_status) { should eq 0 }
+  end
+  
+  # MongoDB cluster setup add node3 to configuration
+  describe command('sleep 25 && mongo --port 27011 --eval="rs.add(\"localhost:27013\")"') do
+    its(:exit_status) { should eq 0 }
+  end
+
+  # MongoDB cluster setup import delicious
+  describe command('mongoimport --port 27011 --db db1 --collection docs < delicious-rss-1250k') do
+    its(:exit_status) { should eq 0 }
+  end
+
+  # MongoDB cluster setup dump node1
+  describe command('mongodump --port 27011 -o dumpfile1') do
+    its(:exit_status) { should eq 0 }
+  end
+
+  # MongoDB cluster setup dump node2
+  describe command('mongodump --port 27012 -o dumpfile1') do
+    its(:exit_status) { should eq 0 }
+  end
+
+  # MongoDB cluster setup dump node3
+  describe command('mongodump --port 27013 -o dumpfile1') do
     its(:exit_status) { should eq 0 }
   end
 
