e049ab69842a4584e2193396d058354eebe739ae
Derek Crudgington <derek@joyent.com>
2016-12-07 09:16:10 -0600

    Updated mysql replication to import data

--- spec/apps/mysql_spec.rb
+++ spec/apps/mysql_spec.rb
@@ -139,5 +139,11 @@ if ! file('/var/lib/mysql').exists?
     describe command('mysql --socket=/var/run/mysqld/mysqldslave.sock -s -e "show slave status \G"') do
       its(:stdout) { should match /Slave_IO_Running: Yes/ }
       its(:stdout) { should match /Slave_SQL_Running: Yes/ }
+    end
+    
+    # mysqlimport to test slave
+    describe command('mysql -uroot -e "create database testslave;" && mysql -uroot testslave < /root/test.sql') do
+      its(:exit_status) { should eq 0 }
+    end
   end
 end
