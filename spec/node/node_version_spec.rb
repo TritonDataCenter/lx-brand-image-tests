5f818294659435e3fd02e25e06cb5a791d71b02d
Christopher Horrell <christopher@joyent.com>
2017-02-27 10:31:57 -0500

    Drop Node.js v0.12

--- spec/node/node_version_spec.rb
+++ spec/node/node_version_spec.rb
@@ -1,12 +1,5 @@
 require 'spec_helper'
 
-# Node 12.x
-if property[:name] =~ /v0.12./
-  describe command('node -v') do
-    its(:stdout) { should contain('v0.12.17') }
-  end
-end
-
 # Node 4.x
 if property[:name] =~ /v4./
   describe command('node -v') do
