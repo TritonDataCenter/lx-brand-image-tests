9874318f79b0c2ef6d7a5e88768e08bc9e9508fe
Christopher Horrell <christopher@joyent.com>
2017-04-03 12:07:11 -0400

    test for makedev apt pin

--- spec/ubuntu/apt-get_spec.rb
+++ spec/ubuntu/apt-get_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
 
+# Make sure we apt pin makedev
+describe file('/etc/apt/preferences.d/makedev') do
+  it { should be_file }
+  its(:content) { should match /Package: makedev/ }
+  its(:content) { should match /Pin: release */ }
+  its(:content) { should match /Pin-Priority: -1/ }
+end
+
 # Testing basic apt-get commands
 describe command('apt-get -y update') do
   its(:exit_status) { should eq 0 }
