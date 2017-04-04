43a8b901848d06a82d374623d5cf33c676495231
Christopher Horrell <christopher@joyent.com>
2017-04-04 10:29:13 -0400

    Update for Node.js v4.8.2 and v6.10.2

--- spec/node/node_version_spec.rb
+++ spec/node/node_version_spec.rb
@@ -3,13 +3,13 @@ require 'spec_helper'
 # Node 4.x
 if property[:name] =~ /v4./
   describe command('node -v') do
-    its(:stdout) { should contain('v4.8.1') }
+    its(:stdout) { should contain('v4.8.2') }
   end
 end
 
 # Node 6.x
 if property[:name] =~ /v6./
   describe command('node -v') do
-    its(:stdout) { should contain('v6.10.1') }
+    its(:stdout) { should contain('v6.10.2') }
   end
 end
