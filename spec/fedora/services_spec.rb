8678761fe60877528dac42fe0a65e0e9c84adab3
Christopher Horrell <christopher@joyent.com>
2017-05-09 14:51:10 -0400

    Drop chronyd and timedatex from fedora test

--- spec/fedora/services_spec.rb
+++ spec/fedora/services_spec.rb
@@ -5,11 +5,6 @@ describe service('crond') do
   it { should be_running }
 end
 
-describe service('chronyd') do
-  it { should be_enabled }
-  it { should be_running }
-end
-
 describe service('rsyslog') do
   it { should be_enabled }
   it { should be_running }
@@ -18,9 +13,3 @@ end
 describe service('sshd') do
   it { should be_enabled }
 end
-
-describe service('timedatex') do
-  it { should be_enabled }
-  it { should be_running }
-end
-
